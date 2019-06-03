b745518e27578d595af34f47d25c212f0d740f80
Wioleta Tyrańska <wtyranska@MacBook-Pro-Hatimeria.local>
2019-06-03 13:24:12 +0200

    Add minor changes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
-var NumberToMeasue = function(value, measurment) {
-    var newMeasure = value + measurment;
-    return newMeasure;
-};
+$( document ).ready(function() {
+    let addUnit = (value, unit) => {
+        return value + unit;
+    };
 
-var headerMover = function () {
-    headerCover = $(".header > .cover");
-    defaultCoverPos = 0;
-    headerCover.css("left", NumberToMeasue(defaultCoverPos, "%"));
-    var onePercentHeight = $(window).outerHeight()/100;
-    $(window).scroll(function(){
-        var scrollTopPos = $(window).scrollTop();
-        var currentPercent = Math.round(scrollTopPos/onePercentHeight) + defaultCoverPos;
-        if (currentPercent <= 105) {
-            headerCover.css("left", NumberToMeasue(currentPercent, "%"));
-        }
-    })
-};
+    let addPercentChar = (value) => {
+        return addUnit(value, "%");
+    };
 
+    let coverElemMover = (CoverElement, defaultCoverPos) => {
+        CoverElement.css("left", addPercentChar(defaultCoverPos));
+        let onePercentHeight = $(window).outerHeight()/100;
+        $(window).scroll(() => {
+            let scrollTopPos = $(window).scrollTop();
+            let currentPercent = Math.round(scrollTopPos/onePercentHeight) + defaultCoverPos;
+            if (currentPercent <= 105) {
+                CoverElement.css("left", addPercentChar(currentPercent));
+            }
+        })
+    };
 
-
-headerMover();
+    let defaultCoverSetting = 0;
+    let headerCoverElement = $(".header > .cover");
+    coverElemMover(headerCoverElement, defaultCoverSetting);
+});
